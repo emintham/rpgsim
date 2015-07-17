d2899f6e963c4dcbef3164aa541a9366cc55af84
emintham <emin@securitycompass.com>
2015-07-17 00:28:36 -0400

    adds attack stats

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 DEFAULT_STATS = {
     'strength': 0,
     'speed': 0,
+    'magic': 0,
     'stamina': 0,
     'hp': 0,
     'mp': 0,
@@ -9,3 +10,9 @@ DEFAULT_STATS = {
 
 MAX_STAT = 100
 DEFAULT_NAME = 'Unnamed Hero'
+
+ATTACK_STATS = [
+    'strength',
+    'speed',
+    'magic',
+]
