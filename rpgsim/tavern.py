d992ecee631eb97e8c746527e093ad07d20b3d65
emintham <emin@securitycompass.com>
2015-07-16 20:53:22 -0400

    support multiple admit/boot in tavern

--- rpgsim/tavern.py
+++ rpgsim/tavern.py
@@ -18,27 +18,35 @@ class Tavern(Borg):
         return person in self._guests
 
     def admit(self, person):
-        """Puts a person into tavern."""
-        if not isinstance(person, Person):
-            error_msg = 'Expecting argument of type {}, got {}'
+        """Puts a person or persons into tavern."""
+        if not isinstance(person, Person) and not hasattr(person, '__iter__'):
+            error_msg = 'Expecting argument of type {} or iterable, got {}'
             raise TypeError(error_msg.format(Person.__name__,
                                              person.__class__.__name__))
 
-        if person not in self._guests:
-            self._guests.add(person)
+        if hasattr(person, '__iter__'):
+            for p in person:
+                self.admit(p)
+        else:
+            if person not in self._guests:
+                self._guests.add(person)
 
     def boot(self, person):
-        """Kick a person out of the tavern."""
-        if not isinstance(person, Person):
-            error_msg = 'Expecting argument of type {}, got {}'
+        """Kick a person or persons out of the tavern."""
+        if not isinstance(person, Person) and not hasattr(person, '__iter__'):
+            error_msg = 'Expecting argument of type {} or iterable, got {}'
             raise TypeError(error_msg.format(Person.__name__,
                                              person.__class__.__name__))
 
-        try:
-            self._guests.remove(person)
-        except KeyError:
-            raise Exception('{} is not in {}'.format(person, Tavern.__name__))
+        if hasattr(person, '__iter__'):
+            for p in person:
+                self.boot(p)
+        else:
+            try:
+                self._guests.remove(person)
+            except KeyError:
+                raise Exception('{} is not in {}'.format(person, Tavern.__name__))
 
     @property
     def guests(self):
-        return list(self._guests)
+        return set(self._guests)
--- tests/test_tavern.py
+++ tests/test_tavern.py
@@ -17,12 +17,25 @@ class TavernTests(TestCase):
         self.assertTrue(self.person in self.tavern)
         self.assertTrue(other not in self.tavern)
 
+    def test_admit_multiple(self):
+        persons = [Person() for _ in range(5)]
+        self.tavern.admit(persons)
+        self.assertEqual(self.tavern.guests, set(persons))
+
     def test_boot(self):
         self.tavern.admit(self.person)
         self.assertTrue(self.person in self.tavern)
 
         self.tavern.boot(self.person)
         self.assertTrue(self.person not in self.tavern)
+
+    def test_boot_multiple(self):
+        persons = [Person() for _ in range(5)]
+        self.tavern.admit(persons)
+
+        self.tavern.boot(persons[:3])
+
+        self.assertEqual(self.tavern.guests, set(persons[3:]))
 
     def test_boot_non_guest_raises_error(self):
         with self.assertRaises(Exception):
@@ -32,3 +45,8 @@ class TavernTests(TestCase):
         t2 = Tavern()
         self.tavern.admit(self.person)
         self.assertTrue(self.person in t2)
+
+    def test_tavern_guests(self):
+        persons = [Person() for _ in range(5)]
+        self.tavern.admit(persons)
+        self.assertEqual(self.tavern.guests, set(persons))
